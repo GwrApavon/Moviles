62d4fd4490115d9ddd88201b80a06a5165853f0f
Ángel Pavón Fraile <bc.apavon.1995@iespabloserrano.com>
2022-10-21 13:45:09 +0200

    Hecho día 21/10

---
+++ P_04_Vistas_03/app/src/main/java/com/example/p_04_vistas_03/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p_04_vistas_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
